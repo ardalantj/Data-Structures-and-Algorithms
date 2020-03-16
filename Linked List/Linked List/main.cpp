3630f62e241666fca2524a1a92de9a43210dae18
Ardalan <ardalantj1107@gmail.com>
2020-03-15 23:18:55 -0400

    Added Remove duplicate and reverse function

--- Linked List/Linked List/main.cpp
+++ Linked List/Linked List/main.cpp
@@ -201,14 +201,63 @@ int Delete(Node *p, int index)
     }
 }
 
+bool isSorted(Node *p)
+{
+    int x = INT32_MIN;
+    
+    while(p != nullptr)
+    {
+       if(p->data < x)
+           return false;
+        x = p->data;
+        p = p->next;
+    }
+    return true;
+}
+
+void RemoveDuplicate(Node *p)
+{
+    Node *q = p->next;
+    while(q != nullptr)
+    {
+        if(p->data != q->data)
+        {
+            p = q;
+            q = q->next;
+        }
+        else
+        {
+            p->next = q->next;
+            delete q;
+            q = p->next;
+        }
+    }
+}
+
+void Reverse(Node *p)
+{
+    Node *q = nullptr;
+    Node *r = nullptr;
+    
+    while(p != nullptr)
+    {
+        r = q;
+        q = p;
+        p = p->next;
+        q->next = r;
+    }
+    first = q;
+}
+
 int main(int argc, const char * argv[]) {
    
-    int A[] = {10,20,30,40};
-    Create(A,4);
+    int A[] = {10,20,20,20,30,40,50};
+    Create(A,7);
     //Display(first);
    // printf("Length is %d", Count(first));
     //SortedInsert(first,2);
-    Delete(first,4);
+    //Delete(first,4);
+    RemoveDuplicate(first);
     Display(first);
     
     return 0;
