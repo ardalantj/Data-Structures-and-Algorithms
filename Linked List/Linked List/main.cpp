e4c7b3be8fc10e8e9066a1d4e363b7a4d41a0818
Ardalan <ardalantj1107@gmail.com>
2020-03-21 11:17:55 -0400

    Added isLoop function

--- Linked List/Linked List/main.cpp
+++ Linked List/Linked List/main.cpp
@@ -320,6 +320,23 @@ void Merge(Node *p, Node *q)
     if(q) last->next = q;
 }
 
+int isLoop(Node *f)
+{
+    Node *p,*q;
+    p = q = f;
+    
+    do
+    {
+        p = p->next;
+        q = q->next;
+        q = q?q->next:q;
+    }while(p && q && p!=q);
+    
+    if(p==q)
+        return 1;
+    else
+        return 0;
+}
 
 int main(int argc, const char * argv[]) {
    
